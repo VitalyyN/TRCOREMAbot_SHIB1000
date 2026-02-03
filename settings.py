94941c39cb6e2a766d949b7abbeb8e87f166cd35
Vitalyy <vitalyy@bk.ru>
2026-02-03 23:16:25 +0300

    chore: Update trading configuration parameters in settings.py

--- settings.py
+++ settings.py
@@ -3,10 +3,10 @@ ONLY_LONG = True
 
 EMA_FAST  = 50
 EMA_SLOW  = 60
-TAKE_PROFIT    = 0.007          # (0,015 это 1.5 %)
+TAKE_PROFIT    = 0.004          # (0,015 это 1.5 %)
 POSITION_SIZE = 0.1             # 10 % от equity (берём позже из биржи)
-DCA_STEP = 0.00002              # $ шаг сетки усреднения
-DCA_GRID = [1, 1, 2, 2]         # коэффициенты усреднения
+DCA_STEP = 0.00001              # $ шаг сетки усреднения
+DCA_GRID = [1, 2, 3, 4]         # коэффициенты усреднения
 
 SYMBOL = "SHIB1000USDT"
 INTERVAL = '1'                  # минуты
